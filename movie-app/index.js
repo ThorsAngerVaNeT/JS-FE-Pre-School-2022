6a89e4bb679b900aafe8036dfc48a9689d946c19
Vadzim Antonau <vanetthor@gmail.com>
2022-02-10 18:59:56 +0300

    feat: add design, refactor js

--- movie-app/index.js
+++ movie-app/index.js
@@ -1,4 +1,4 @@
-import { API_CONF, API_KEY, API_URL } from './js/api-config.js';
+import { API_CONF, API_URL, SEARCH_URL } from './js/api-config.js';
 import data from './movies.js';
 
 const searchForm = document.getElementById('search-form');
@@ -6,41 +6,35 @@ const search = document.getElementById('search');
 const main = document.querySelector('.main');
 
 function fetchAPI() {
-  fetch(`${API_URL}discover/movie/?sort_by=popularity.desc&api_key=${API_KEY}&page=1`)
+  fetch(`${API_URL}`)
     .then(res => res.json())
     .then(json => insertMovieDivs(json))
     .catch(err => console.log(err));
 }
-//https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query=%22captain
-insertMovieDivs(data);
+
+fetchAPI();
 function insertMovieDivs(json) {
-  // console.log(json);
   main.innerHTML = '';
   if (json.results.length === 0) main.innerHTML = 'Nothing Found!';
   else
     json.results.forEach(movie => {
-      let divMovie = document.createElement('div');
-      divMovie.classList.add('movie-card');
-
-      let imgMovie = document.createElement('img');
-      imgMovie.src = `${API_CONF.images.base_url}/w780/${movie.poster_path}`;
-      imgMovie.alt = `${movie.title}`;
-      divMovie.appendChild(imgMovie);
-
-      let divInfo = document.createElement('div');
-      divInfo.classList.add('info');
-      let title = document.createElement('h3');
-      title.textContent = movie.title;
-      let score = document.createElement('span');
-      score.textContent = movie.vote_average;
-      divInfo.appendChild(title);
-      divInfo.appendChild(score);
-      divMovie.appendChild(divInfo);
+      const divMovie = `<div class="movie-card">
+          <img src="${API_CONF.images.base_url}w780${movie.poster_path}" alt="${movie.title}">
+          <div class="info">
+            <div class="info-title">
+              <h3>${movie.title}</h3> (${new Date(movie.release_date).getFullYear()})
+            </div>
+            <div class="info-score">
+              <i class="fa-solid fa-star"></i>
+              <span>${movie.vote_average}</span>
+            </div>
+          </div>
+        </div>`;
 
       let divOverview = document.create;
 
-      // TODO add poster, title, score, desc
-      main.appendChild(divMovie);
+      // TODO add desc
+      main.insertAdjacentHTML('beforeend', divMovie);
     });
 }
 
@@ -48,7 +42,7 @@ function searchAPI(e) {
   e.preventDefault();
 
   if (search.value !== '') {
-    fetch(`${API_URL}search/movie/?sort_by=popularity.desc&api_key=${API_KEY}&query=${search.value}`)
+    fetch(`${SEARCH_URL}${search.value}`)
       .then(res => res.json())
       .then(json => insertMovieDivs(json))
       .catch(err => console.log(err));
@@ -56,20 +50,3 @@ function searchAPI(e) {
 }
 
 searchForm.addEventListener('submit', searchAPI);
-
-// fetchAPI();
-
-// adult: false
-// backdrop_path: "/iQFcwSGbZXMkeyKrxbPnwnRo5fl.jpg"
-// genre_ids: (3) [28, 12, 878]
-// id: 634649
-// original_language: "en"
-// original_title: "Spider-Man: No Way Home"
-// overview: "Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero. When he asks for help from Doctor Strange the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man."
-// popularity: 11286.376
-// poster_path: "/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg"
-// release_date: "2021-12-15"
-// title: "Spider-Man: No Way Home"
-// video: false
-// vote_average: 8.4
-// vote_count: 7408
